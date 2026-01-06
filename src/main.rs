f82c903e5ab4b2749cdd0c9350e7c93c8706c8a7
Daniel Pérez <steew@psi.my.domain>
2026-01-06 23:07:34 +0100

    Formatting, started working on config file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::HashMap;
 use std::collections::VecDeque;
+use std::str::FromStr;
 use std::sync::Arc;
 
 use futures_util::StreamExt;
+use ini::Ini;
 use irc::client::Sender;
 use irc::client::data::Config;
 use irc::proto::Command;
@@ -12,7 +15,6 @@ use serenity::all::Message;
 use serenity::all::Ready;
 use serenity::all::Webhook;
 use serenity::async_trait;
-use serenity::model::webhook;
 use serenity::prelude::*;
 use tokio::spawn;
 use tokio::sync::Notify;
@@ -20,8 +22,8 @@ use tokio::sync::Notify;
 #[derive(Debug)]
 enum RelayDirection {
     INVALID,
-    IRC2DIS,
-    DIS2IRC,
+    IRC2DIS(String),
+    DIS2IRC(ChannelId),
 }
 
 struct RelayMessage {
@@ -42,6 +44,22 @@ impl Default for RelayNotify {
     fn default() -> Self {
         RelayNotify {
             notify: Notify::new(),
+        }
+    }
+}
+
+struct RelayAssoc {
+    // stores the Discord - IRC channel bridge associations
+    bridge_assoc: HashMap<ChannelId, Vec<String>>,
+    // stores the webhook URL for the discord channels
+    chid_webhook_assoc: HashMap<ChannelId, String>,
+}
+
+impl Default for RelayAssoc {
+    fn default() -> Self {
+        RelayAssoc {
+            bridge_assoc: HashMap::new(),
+            chid_webhook_assoc: HashMap::new()
         }
     }
 }
@@ -90,7 +108,7 @@ impl EventHandler for Handler {
             // create a new message with the received discord message contents
             let mut new_message = RelayMessage::default();
             new_message.contents = msg.content;
-            new_message.direction = RelayDirection::DIS2IRC;
+            new_message.direction = RelayDirection::DIS2IRC(msg.channel_id);
             new_message.author = msg.author.name;
             // push the pending message to the relay buffer
             relay_buffer.pending_relay_messages.push_back(new_message);
@@ -113,6 +131,7 @@ async fn relay_consumer(
     http: Arc<Http>,
     webhook: Webhook,
     sender: Sender,
+    assoc: RelayAssoc
 ) {
     loop {
         // await for new relay pending events
@@ -127,13 +146,13 @@ async fn relay_consumer(
             pending.contents, pending.direction
         );
         match pending.direction {
-            RelayDirection::IRC2DIS => {
+            RelayDirection::IRC2DIS(chan) => {
                 // let chanid = ChannelId::new(591954698664149044);
                 // chanid.say(http.clone(), pending.contents).await.unwrap();
                 let builder = ExecuteWebhook::new().content(pending.contents).username(pending.author);
                 webhook.execute(&http, false, builder).await.expect("Could not execute webhook.");
             }
-            RelayDirection::DIS2IRC => {
+            RelayDirection::DIS2IRC(chan) => {
                 let unpingable_name = pending.author.clone();
                 let (first, rest) = unpingable_name.split_at(1);
                 let mut unpingable_name = String::new();
@@ -149,62 +168,12 @@ async fn relay_consumer(
     }
 }
 
-#[tokio::main]
-async fn main() {
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
-    let mut discord_client = serenity::Client::builder(&token, intents)
-        .event_handler(Handler)
-        .await
-        .expect("Err creating client");
-
-    let buffer_reference = Arc::new(RwLock::new(MessageBuffer::default()));
-    let notify = Arc::new(RelayNotify::default());
-
-    // set the arbitrary data mutex as write
-    {
-        let mut data = discord_client.data.write().await;
-        data.insert::<MessageBuffer>(buffer_reference.clone());
-        data.insert::<RelayNotify>(notify.clone());
-    }
-
-    let shared_http = discord_client.http.clone();
-    let shared_notify = notify.clone();
-    let shared_buffer_reference = buffer_reference.clone();
-
-    let webhook = Webhook::from_url(&shared_http, "").await.unwrap();
-
-    // spawn discord client
-    let _client_handle = spawn(async move {
-        if let Err(why) = discord_client.start().await {
-            eprintln!("Client error: {why:?}");
-        }
-    });
-
-    let config = Config {
-        nickname: Some("belltoll".to_owned()),
-        server: Some("irc.libera.chat".to_owned()),
-        channels: vec!["##steew".to_owned()],
-        ..Default::default()
-    };
-
-    let mut irc_client = irc::client::Client::from_config(config).await.unwrap();
-    irc_client.identify().unwrap();
-
+async fn irc_producer(
+    mut irc_client: irc::client::Client,
+    buffer_reference: Arc<RwLock<MessageBuffer>>,
+    notify: Arc<RelayNotify>
+) {
     let mut irc_stream: irc::client::ClientStream = irc_client.stream().unwrap();
-
-    // spawn relay consumer
-    let _relay_handle = spawn(async move {
-        relay_consumer(
-            shared_buffer_reference,
-            shared_notify,
-            shared_http,
-            webhook,
-            irc_client.sender(),
-        )
-        .await;
-    });
 
     while let Ok(Some(message)) = irc_stream.next().await.transpose() {
         let msg_clone = message.clone();
@@ -215,7 +184,7 @@ async fn main() {
                     let mut buffer = buffer_reference.write().await;
                     let mut new_message = RelayMessage::default();
                     new_message.contents = contents;
-                    new_message.direction = RelayDirection::IRC2DIS;
+                    new_message.direction = RelayDirection::IRC2DIS(String::from_str(msg_clone.response_target().unwrap()).unwrap());
                     new_message.author.push_str(uname);
                     buffer.pending_relay_messages.push_back(new_message);
                     notify.notify.notify_one();
@@ -225,4 +194,100 @@ async fn main() {
             _ => {}
         }
     }
+
+}
+#[tokio::main]
+async fn main() {
+    // Shared data initialization
+    // ======================================================================================
+    let buffer_reference = Arc::new(RwLock::new(MessageBuffer::default()));
+    let notify = Arc::new(RelayNotify::default());
+    
+    let discord_notify = notify.clone();
+    let discord_buffer_reference = buffer_reference.clone();
+    let irc_notify = notify.clone();
+    let irc_buffer_reference = buffer_reference.clone();
+
+    // Discord initialization
+    // ======================================================================================
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+    let mut discord_client = serenity::Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    // set the arbitrary data mutex as write
+    {
+        let mut data = discord_client.data.write().await;
+        data.insert::<MessageBuffer>(discord_buffer_reference);
+        data.insert::<RelayNotify>(discord_notify);
+    }
+    
+    // shared http sender for the discord client
+    let shared_http = discord_client.http.clone();
+    
+    // spawn discord client async thread
+    let _client_handle = spawn(async move {
+        if let Err(why) = discord_client.start().await {
+            eprintln!("Client error: {why:?}");
+        }
+    });
+
+    // TODO: remove and read from file
+    let webhook = Webhook::from_url(&shared_http,
+                                    "").await.unwrap();
+
+    // ======================================================================================
+    // IRC initialization
+    let config = Config {
+        nickname: Some("belltoll".to_owned()),
+        server: Some("irc.libera.chat".to_owned()),
+        channels: vec!["##steew".to_owned()],
+        ..Default::default()
+    };
+
+    let irc_client = irc::client::Client::from_config(config).await.unwrap();
+    irc_client.identify().unwrap();
+    let irc_sender = irc_client.sender().clone();
+
+    let _irc_handle = spawn(async move {
+        irc_producer(irc_client, irc_buffer_reference, irc_notify).await;
+    });
+    // Configuration file read for channel bridge associations
+    // ======================================================================================
+    let ini_conf_file = "config.ini";
+    let ini = Ini::load_from_file(ini_conf_file).expect("A config.ini file is needed");
+
+    let mut assoc = RelayAssoc::default();
+        
+    match ini.section(Some("assoc")) {
+        Some(section_contents) => {
+            for (d_channel, i_channel) in section_contents.iter() {
+                let chid = u64::from_str(d_channel).expect("Channel ID is not valid! {d_channel}");
+                let irc_chan = String::from_str(i_channel).expect("Channel name is not valid! {i_channel}");
+                let mut ircs = Vec::new();
+                ircs.push(irc_chan);
+                assoc.bridge_assoc.insert(ChannelId::new(chid), ircs);
+            }
+        },
+        None => { panic!("Expected an [assoc] section with bridge associations.") },
+    }
+    println!("{:?}", assoc.bridge_assoc.keys());
+    println!("{:?}", assoc.bridge_assoc.values());
+
+    // ======================================================================================
+    // Relay consumer thread spawn
+    let _relay_handle = spawn(async move {
+        relay_consumer(
+            buffer_reference,
+            notify,
+            shared_http,
+            webhook,
+            irc_sender,
+            assoc
+        )
+        .await;
+    });
 }
